e679e63508f97633f671771a6c0432e4a66e796c
ACHYUTH CHOWDARY D A <88281033+Achyuthc@users.noreply.github.com>
2022-05-05 20:33:04 +0530

    Create Number of Substrings Containing All Three Characters(LeetCode1358).cpp

---
+++ Number of Substrings Containing All Three Characters(LeetCode1358).cpp
@@ -0,0 +1,28 @@
+/*Given a string s consisting only of characters a, b and c.
+
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+ */
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+     int a[3]={0,0,0};
+     int j=0,n=s.size(),i=0;
+     int ans=0;
+     while(j<s.size())
+     {
+         a[s[j]-'a']++;
+         
+        while(i<n&&a[0]>0&&a[1]>0&&a[2]>0)
+         {
+            
+            a[s[i]-'a']--;
+            i++;
+         }
+         ans+=i;
+        j++;
+     }
+    return ans;
+    }
+};
